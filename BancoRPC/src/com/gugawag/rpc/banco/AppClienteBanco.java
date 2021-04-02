1e386e69f6807e33721f6ad65bb2059061bec305
Karoline Lima <karol.lima012@gmail.com>
2021-04-01 22:02:57 -0300

    Prática - RPC com RMI

---
+++ BancoRPC/src/com/gugawag/rpc/banco/AppClienteBanco.java
@@ -0,0 +1,65 @@
+
+package com.gugawag.rpc.banco;
+
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.util.Scanner;
+
+public class AppClienteBanco {
+
+    public static void main(String[] args) throws RemoteException, NotBoundException, MalformedURLException {
+        // procura o serviço no RMI Registry local. Perceba que o cliente não connhece a implementação do servidor,
+        // apenas a interface
+        Registry registry = LocateRegistry.getRegistry("127.0.0.1", 1099);
+        BancoServiceIF banco = (BancoServiceIF) registry.lookup("BancoService");
+
+        menu();
+        Scanner entrada = new Scanner(System.in);
+        int opcao = entrada.nextInt();
+
+        while(opcao != 9) {
+            switch (opcao) {
+                case 1: {
+                    System.out.println("Digite o número da conta:");
+                    String conta = entrada.next();
+                    //chamada ao método remoto, como se fosse executar localmente
+                    System.out.println(banco.saldo(conta));
+                }
+                case 2: {
+                    //chamada ao método remoto, como se fosse executar localmente
+                    System.out.println(banco.quantidadeContas());
+                }
+                case 4: {
+                    //criar nova conta
+                    System.out.println("--- Cadastro de Conta ---");
+                    System.out.println("Digite o número da conta:");
+                    String numero = entrada.next();
+
+                    System.out.println("Digite o saldo da conta:");
+                    double saldo = entrada.nextDouble();
+                    Conta conta = new Conta(numero,saldo);
+                    banco.adicionarConta(conta);
+                    System.out.println("Conta criada com sucesso!");
+                    System.out.println(conta.toString());
+
+                }
+
+            }
+            menu();
+            opcao = entrada.nextInt();
+        }
+    }
+
+    public static void menu() {
+        System.out.println("\n=== BANCO RMI (ou FMI?!) ===");
+        System.out.println("1 - Saldo da conta");
+        System.out.println("2 - Quantidade de contas");
+        System.out.println("4 - Criar conta");
+        System.out.println("9 - Sair");
+    }
+
+}
